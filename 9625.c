bf70d0f02ada6ef26a592b06e4e1f0bd458f16b2
kdh949 <kdh949@naver.com>
2024-01-25 14:37:44 +0900

    9625+타이머

--- 9625.c
+++ 9625.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
+    int TIME = 0;
+
+    /* Timer on */
+	clock_t start = clock();
+
+
     int num;
-    int a=1, b=0;
-    int temp_a, temp_b;
+    long long a=1, b=0;
+    long long temp_a, temp_b;
 
     scanf("%d", &num);
+
+    int light = num*num;
 
     temp_a = a;
     temp_b = b;
@@ -15,19 +24,19 @@ int main(){
 
     for(int i=0; i<num; i++){
         while(temp_a != 0 || temp_b != 0){
-            if(temp_a > 30){
-                a-=30;
-                b+=30;
-                temp_a-=30;
+            if(temp_a > light){
+                a-=light;
+                b+=light;
+                temp_a-=light;
             }
             else if(temp_a > 0){
                 a--;
                 b++;
                 temp_a--;
             }
-            else if(temp_b > 30){
-                a+=30;
-                temp_b-=30;
+            else if(temp_b > light){
+                a+=light;
+                temp_b-=light;
             }
             else if(temp_b > 0){
                 a++;
@@ -39,6 +48,12 @@ int main(){
         temp_b = b;
     }
 
-    printf("%d %d", a, b);
+    printf("%lld %lld", a, b);
+
+    /* Timer off */
+	TIME += ((int)clock() - start) / (CLOCKS_PER_SEC / 1000);
+    
+	printf("\nTIME : %d ms\n", TIME); /* ms 단위로 출력 */
+
     return 0;
 }
