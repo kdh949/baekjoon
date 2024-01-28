73585eb9deb10e988151d9a223b16a4034d8f88a
kdh949 <kdh949@naver.com>
2024-01-29 01:10:49 +0900

    1018 진행중

---
+++ 1018(pending).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    char chess[51][51];
+
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    //체스판 상황 입력받기  
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            scanf("%c", &chess[i][j]);
+        }
+    }
+
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            if(chess[i][])
+
+
+
+        }
+    }
+    
+    
+
+}
