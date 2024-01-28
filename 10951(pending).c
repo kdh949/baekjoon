dc1f5470f7e752420419d3729b643151d8e9d243
kdh949 <kdh949@naver.com>
2024-01-29 01:04:15 +0900

    pending

---
+++ 10951(pending).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char a[3], b[3];
+
+    while (1) {
+        
+    }
+    
+}
---
+++ 15740(pending).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    long long n1, n2;
+    
+    scanf("%lld %lld", &n1, &n2);
+
+    printf("%lld", n1+n2);
+
+    return 0;
+}
